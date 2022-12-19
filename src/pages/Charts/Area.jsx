a8f636e80a7f58a4d3458128fda51bf6a819eef3
hacenbarb <cenha06@gmail.com>
2022-12-19 14:05:32 +0100

    bar chart

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -40,7 +40,6 @@ const Area = () => {
         </SeriesCollectionDirective>
       </ChartComponent>
       </div>
-
     </div>
   );
 };
--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  BarSeries,
+} from "@syncfusion/ej2-react-charts";
 
+// import {
+//   ChartComponent,
+//   SeriesCollectionDirective,
+//   SeriesDirective,
+//   Inject,
+//   Legend,
+//   Category,
+//   Tooltip,
+//   DataLabel,
+//   BarSeries,
+// } from "@syncfusion/ej2-react-charts";
+
+import {
+  barCustomSeries,
+  barPrimaryXAxis,
+  barPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { Header } from "../../components";
+import { Category } from "@syncfusion/ej2/charts";
 const Bar = () => {
+  const { currentMode } = useStateContext();
   return (
-    <div>Bar</div>
-  )
-}
+    <div>
+      <Header category="Bar" title="Inflation Rate" />
+      <div className="w-full">
+        <ChartComponent
+          id="bar-chart"
+          height="420px"
+          primaryXAxis={barPrimaryXAxis}
+          primaryYAxis={barPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true }}
+          background={currentMode === "Dark" ? "#33373E" : "#fff"}
+          legendSettings={{ background: "white" }}
+        >
+          <Inject services={[BarSeries, DateTime, Legend]} />
+          <SeriesCollectionDirective>
+            {barCustomSeries.map((item, index) => (
+              <SeriesDirective key={index} {...item} />
+            ))}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
+};
 
-export default Bar
+export default Bar;
