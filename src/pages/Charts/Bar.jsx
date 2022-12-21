25db551b1a740e8fa5b88a429e64895407d82d76
hacenbarb <cenha06@gmail.com>
2022-12-21 16:56:28 +0100

    add charts

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,42 +1,29 @@
-import React from "react";
-import {
-  ChartComponent,
-  SeriesCollectionDirective,
-  SeriesDirective,
-  Inject,
-  DateTime,
-  Legend,
-  BarSeries,
-} from "@syncfusion/ej2-react-charts";
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip, ColumnSeries, DataLabel } from '@syncfusion/ej2-react-charts';
 
-import {
-  barCustomSeries,
-  barPrimaryXAxis,
-  barPrimaryYAxis,
-} from "../../data/dummy";
-import { useStateContext } from "../../contexts/ContextProvider";
-import { Header } from "../../components";
+import { barCustomSeries, barPrimaryXAxis, barPrimaryYAxis } from '../../data/dummy';
+import { ChartsHeader } from '../../components';
+import { useStateContext } from '../../contexts/ContextProvider';
+
 const Bar = () => {
   const { currentMode } = useStateContext();
+
   return (
-    <div>
-      <Header category="Bar" title="Inflation Rate" />
-      <div className="w-full">
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <ChartsHeader category="Bar" title="Olympic Medal Counts - RIO" />
+      <div className=" w-full">
         <ChartComponent
-          id="bar-chart"
-          height="420px"
+          id="charts"
           primaryXAxis={barPrimaryXAxis}
           primaryYAxis={barPrimaryYAxis}
           chartArea={{ border: { width: 0 } }}
           tooltip={{ enable: true }}
-          background={currentMode === "Dark" ? "#33373E" : "#fff"}
-          legendSettings={{ background: "white" }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+          legendSettings={{ background: 'white' }}
         >
-          <Inject services={[BarSeries, DateTime, Legend]} />
+          <Inject services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]} />
           <SeriesCollectionDirective>
-            {barCustomSeries.map((item, index) => (
-              <SeriesDirective key={index} {...item} />
-            ))}
+            {barCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
           </SeriesCollectionDirective>
         </ChartComponent>
       </div>
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import {LineChart, Header} from "../../components"
-const Line = () => {
-  return (
-    <div>
-      <Header category="Chart" title="Inflation Rate"/>
-      <div className="w-full">
-        <LineChart />
-      </div>
-    </div>
-  )
-}
+import React from 'react';
 
-export default Line
+import { ChartsHeader, LineChart } from '../../components';
+
+const Line = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <ChartsHeader category="Line" title="Inflation Rate" />
+    <div className="w-full">
+      <LineChart />
+    </div>
+  </div>
+);
+
+export default Line;
